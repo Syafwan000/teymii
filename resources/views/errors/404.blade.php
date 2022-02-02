dbf529c48580c9badf107102da4cba2aa32f3a2d
Syafwan000 <muhammadsyafwan04@gmail.com>
2022-02-02 11:54:32 +0700

    TeyMii: 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/error.css') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <title>TeyMii - Kesalahan 404</title>
+</head>
+<body>
+    <div class="container position-absolute top-50 start-50 translate-middle text-center">
+        <h1 class="error-404">404</h1>
+        <h3 class="error-title">Halaman Tidak Ditemukan</h3>
+        <p class="error-text">Pastikan URL yang anda kunjungi valid</p>
+        <a class="btn btn-black shadow-none" href="/">Kembali</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
