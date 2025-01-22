6bfea70c94904830a60717d9f2177c985d0f8c3a
Syafwan000 <muhammadsyafwan04@gmail.com>
2025-01-22 09:58:09 +0700

    chore: clipboard js for copying url

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
+import ClipboardJS from 'clipboard'
 import './bootstrap'
+
+if (document.querySelector('#share-link')) { new ClipboardJS('#share-link') }
+document.addEventListener('alpine:init', () => {
+    Alpine.data('share', () => ({
+        text: 'Share',
+        clicked: false,
+
+        copyLink() {
+            if (this.clicked) return
+            this.text = 'Copied'
+            this.clicked = true
+            setTimeout(() => {
+                this.text = 'Share'
+                this.clicked = false
+            }, 1000)
+        }
+    }))
+})
--- resources/views/components/inputs/share.blade.php
+++ resources/views/components/inputs/share.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-center items-center gap-4">
-    <button type="button" class="flex items-center gap-1 text-sm font-medium bg-gray-200 rounded-md px-3 py-1 hover:bg-gray-300">
+    <button x-data="share" @click="copyLink" :disabled="clicked" type="button" id="share-link" data-clipboard-text="{{ url()->current() }}" class="min-w-24 transition-all duration-300 ease-in-out flex justify-center items-center gap-1 text-sm font-medium bg-gray-200 rounded-md px-3 py-1 hover:bg-gray-300 focus:bg-blue-600 focus:text-white disabled:bg-blue-600 disabled:text-white">
         <span class="icon-[mdi--link-variant] w-5 h-5"></span>
-        Share
+        <span x-text="text"></span>
     </button>
     <span class="font-semibold">|</span>
     <div class="flex items-center gap-1.5">
