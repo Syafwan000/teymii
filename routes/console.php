7514fa48f917c431968ed2954962befdf9133a79
Syafwan000 <muhammadsyafwan04@gmail.com>
2025-03-06 13:46:25 +0700

    chore: change clear:message command schedule from every minute to daily

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('clear:message')->everyMinute();
+Schedule::command('clear:message')->daily();
